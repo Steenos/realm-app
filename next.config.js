72d7c064d3cccf4b7cc9bfde982319a1ae7158cd
Steenos <steeeenos@gmail.com>
2022-02-05 15:38:20 -0500

    updz12

--- next.config.js
+++ next.config.js
@@ -55,12 +55,12 @@ const nextConfig = {
   },
 };
 
-/* const header = {
+ const header = {
   async headers() {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
@@ -71,25 +71,11 @@ const nextConfig = {
     ]
   },
   //withPlugins(plugins, nextConfig)
-};  */
+};  
 
 
 
 // next.config.js
 
 
-module.exports = withPlugins(plugins, nextConfig, {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "https://www.artrealms.io/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }});
+module.exports = withPlugins(plugins, nextConfig, header);
