231a7ad88a6cba547c9e16d82104ebecd68a13c4
Steenos <steeeenos@gmail.com>
2022-02-05 13:13:56 -0500

    upd6

--- next.config.js
+++ next.config.js
@@ -86,7 +86,7 @@ module.exports = withPlugins(plugins, nextConfig, {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://www.artrealms.io/:path*" },
+          { key: "Access-Control-Allow-Origin", value: "https://www.artrealms.io" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
