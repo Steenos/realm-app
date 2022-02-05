c1c52a1198737c7935621dd13ea064c42214b777
Steenos <steeeenos@gmail.com>
2022-02-05 11:22:41 -0500

    cors middlewear again

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,15 +1,22 @@
-import type { NextRequest } from 'next/server'
-import cors from 'lib/cors'
 
-export async function middleware(req: NextRequest) {
-  // `cors` also takes care of handling OPTIONS requests
-  return cors(
-    req,
-    new Response(JSON.stringify({ message: 'Hello World!' }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    })
-  )
+import Cors from 'cors'
+import initMiddleware from '/src/lib/init-middlewear'
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res)
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
 }
 
 
