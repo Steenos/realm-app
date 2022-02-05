c9b9e6db534d065a76e360320ea46a38ec06e556
Steenos <steeeenos@gmail.com>
2022-02-04 21:42:59 -0500

    newt tryds

--- next.config.js
+++ next.config.js
@@ -80,13 +80,18 @@ const nextConfig = {
 
 
 module.exports = withPlugins(plugins, nextConfig, {
-  async rewrites() {
-      return [
-        {
-          source: '/#/:path*',
-          destination: 'https://artrealms.io/:path*',
-        },
-      ]
-    }
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://artrealms.io" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
-    );
+});
