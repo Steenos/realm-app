6a2100a031107f526ecd3ebe55739a504f23871e
Steenos <steeeenos@gmail.com>
2022-02-05 17:30:44 -0500

    moved vercel.json

--- next.config.js
+++ next.config.js
@@ -60,7 +60,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "https://www.artrealms.io/*",
+        source: "https://artrealms.io/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
@@ -69,7 +69,7 @@ const nextConfig = {
         ]
       }
     ]
-  },
+  }
   //withPlugins(plugins, nextConfig)
 };  
 
