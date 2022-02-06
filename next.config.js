371ec7fdc1f12de6bfdca42a6aea83b5654b24a5
Steenos <steeeenos@gmail.com>
2022-02-06 13:47:48 -0500

    upd299

--- next.config.js
+++ next.config.js
@@ -60,7 +60,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "artrealms.io/:path*",
+        source: "https://artrealms.io/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
