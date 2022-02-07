f6fb50df3b9ee340e3e7d0577c06fd63b3bee27f
Steenos <steeeenos@gmail.com>
2022-02-07 15:08:49 -0500

    dfg api

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -10,4 +10,10 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   res.status(200).json({ name: 'John Doe' })
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
 }
